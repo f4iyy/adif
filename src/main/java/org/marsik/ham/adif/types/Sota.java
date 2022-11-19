4424afb546c4b034e937ef2805e862808042e11d
F4IYY Leopold Vallauri <95191806+f4iyy@users.noreply.github.com>
2022-11-19 06:57:08 +0100

    Complies (try to!) with SOTA reference format A/BB-CCC

--- src/main/java/org/marsik/ham/adif/types/Sota.java
+++ src/main/java/org/marsik/ham/adif/types/Sota.java
@@ -20,7 +20,7 @@ public class Sota implements AdifEnumCode {
         return ituPrefix + (subdivision != null ? "/" + subdivision : "") + "-" + String.format("%03d", reference);
     }
 
-    private static Pattern SOTA_RE = Pattern.compile("([A-Z][A-Z])(/[A-Z][A-Z])-([0-9]+)", Pattern.CASE_INSENSITIVE);
+    private static Pattern SOTA_RE = Pattern.compile("([A-Z]){1,3}\/([A-Z]{2})-([0-9]{3})", Pattern.CASE_INSENSITIVE);
 
     public static Sota valueOf(String s) {
         Matcher m = SOTA_RE.matcher(s);
