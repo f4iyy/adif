20ff6c86ce2f13a5676cd9785826789dca817fb4
F4IYY Leopold Vallauri <95191806+f4iyy@users.noreply.github.com>
2022-11-19 07:12:30 +0100

    Update reg & used groups

--- src/main/java/org/marsik/ham/adif/types/Sota.java
+++ src/main/java/org/marsik/ham/adif/types/Sota.java
@@ -20,12 +20,12 @@ public class Sota implements AdifEnumCode {
         return ituPrefix + (subdivision != null ? "/" + subdivision : "") + "-" + String.format("%03d", reference);
     }
 
-    private static Pattern SOTA_RE = Pattern.compile("([A-Z]){1,3}/([A-Z]{2})-([0-9]{3})", Pattern.CASE_INSENSITIVE);
+    private static Pattern SOTA_RE = Pattern.compile("([A-Z]){1,3}/([A-Z]{2})-([0-9]{3})?", Pattern.CASE_INSENSITIVE);
 
     public static Sota valueOf(String s) {
         Matcher m = SOTA_RE.matcher(s);
         if (m.matches()) {
-            return new Sota(m.group(0), m.group(1), Integer.parseInt(m.group(2)));
+            return new Sota(m.group(1), m.group(2), Integer.parseInt(m.group(3)));
         } else {
             throw new IllegalArgumentException("This is not a SOTA reference");
         }
